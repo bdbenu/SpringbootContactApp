5b4e9e401c90389264198c7dc6035d4341670223
bdbenu <bdbenu18@gmail.com>
2023-06-11 20:28:46 +0530

    modify

--- PhoneBookApplication/src/main/java/com/bd/benu/exception/ContaactNotfoundException.java
+++ PhoneBookApplication/src/main/java/com/bd/benu/exception/ContaactNotfoundException.java
@@ -1,4 +1,4 @@
-package com.bd.benu.service;
+package com.bd.benu.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
--- PhoneBookApplication/src/main/java/com/bd/benu/service/ContactServiceImpl.java
+++ PhoneBookApplication/src/main/java/com/bd/benu/service/ContactServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bd.benu.exception.ContaactNotfoundException;
 import com.bd.benu.model.Contact;
 import com.bd.benu.repositary.IContactRepositary;
 
